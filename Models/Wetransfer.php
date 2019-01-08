5f8edd2e965aedc481f68ce17fd017ddd8739d5f
Neareeves <alethesj@gmail.com>
2019-01-08 11:18:31 +0100

    ok

--- Models/Wetransfer.php
+++ Models/Wetransfer.php
@@ -4,11 +4,8 @@
 include('Models/ConnectionBDD.php');
 
 
-<<<<<<< HEAD
-function insertInfo() {
-=======
 function InsertInfo($nom, $destinataire,$expediteur,$message) {
->>>>>>> 2d42dca83d4fc4c81cffa0f32338dc536f075c79
+	
     global $dbh;
 
     $file = $dbh->prepare('INSERT INTO `wetransfer`(`nom_exp`, `destinataire`, ` expediteur`, `message`, `date_ajout`) VALUES (?,?,?,?,NOW())');
@@ -18,4 +15,5 @@ function InsertInfo($nom, $destinataire,$expediteur,$message) {
     // ajouter les informations (`chemin_local`, `chemin_absolu`,)sont les fichiers téléchargés dans execute
 
     return $file->fetchAll();
+}
 
